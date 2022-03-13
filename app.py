da508b4aef108ea725e248acee73bf42ae2f8352
aisligrace <96492230+aisligrace@users.noreply.github.com>
2022-03-13 11:41:52 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
+
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
@@ -20,4 +21,4 @@ def scrape():
    return redirect('/', code=302)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
